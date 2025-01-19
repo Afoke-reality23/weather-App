18ed25b5426d6a9c32370b16a5a18ae50f73b730
Reality <ighoafokereality1@gmail.com>
2025-01-19 02:57:34 +0100

    fourth pc commit

--- app.js
+++ app.js
@@ -7,133 +7,131 @@ const mainImg = document.querySelector(".condition_img");
 const subImg = document.querySelector(".sub_condition_img");
 const tempImg = document.querySelector(".current_temp_img");
 const condText = document.querySelector(".condition_text");
-const temp=document.querySelector('.temp')
-const hoursDiv = document.querySelector('.hours')
-const wind=document.querySelector('.wind')
-const pressure=document.querySelector('.pressure')
-const humidity=document.querySelector('.humidity')
+const temp = document.querySelector(".temp");
+const hoursDiv = document.querySelector(".hours");
+const wind = document.querySelector(".wind");
+const pressure = document.querySelector(".pressure");
+const humidity = document.querySelector(".humidity");
 const hourlyForecast = document.querySelector(".next_23_hours");
-const loader=document.querySelector('.loader')
+const loader = document.querySelector(".loader");
 const dailyForecast = document.querySelector(".daily_forecast");
-const mapLayer=document.getElementById('map')
-const content=document.querySelector('.main_content')
-
-
+const mapLayer = document.getElementById("map");
+const content = document.querySelector(".main_content");
+console.log(content);
 
 btn.addEventListener("click", () => {
-    fetch(`http://localhost:1998?client_port=${window.location.port}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "text/plain",
-        },
-        body: search.value,
-    })
-    loader.addClassList("show_loader")
+  loader.classList.toggle("show");
+  content.classList.toggle("hide");
+
+  fetch(`http://localhost:1998?client_port=${window.location.port}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: search.value,
+  })
     .then((response) => {
-        if (response.ok) {
-            if (response.headers.get("Content-Type") === "application/json") {
-                return response.json();
-            } else {
-                return response.text();
-            }
+      loader.classList.toggle("show");
+      content.classList.toggle("hide");
+      if (response.ok) {
+        if (response.headers.get("Content-Type") === "application/json") {
+          return response.json();
+        } else {
+          return response.text();
         }
-        console.log("ok")
-        loader.removeClassList("show_loader")
-        main_content.style.display='block'
+      }
     })
     .then((data) => {
-        let hourlyForecastData = data.forecaste_response.forecast.forecastday;
-        let condition = data.current_response.current
-        let lat=data.forecaste_response.location.lat
-        let lon=data.forecaste_response.location.lon
-        setCordinates(lat,lon)
-        setDayAndTime(condition);
-        populateAirCondition(hourlyForecastData)
-        createHourlyDiv(hourlyForecastData);
+      let hourlyForecastData = data.forecaste_response.forecast.forecastday;
+      let condition = data.current_response.current;
+      let lat = data.forecaste_response.location.lat;
+      let lon = data.forecaste_response.location.lon;
+      setCordinates(lat, lon);
+      setDayAndTime(condition);
+      populateAirCondition(condition);
+      createHourlyDiv(hourlyForecastData);
     })
     .catch((error) => {
-        console.error("Failed to fetch:", error);
+      console.error("Failed to fetch:", error);
     });
 });
 
-
-function setCordinates(lat,lon){
-    if (currentMarker){
-        map.removeLayer(currentMarker)
-    }
-    map.setView([lat,lon])
-    currentMarker=L.marker([lat,lon]).addTo(map)
+function setCordinates(lat, lon) {
+  if (currentMarker) {
+    map.removeLayer(currentMarker);
+  }
+  map.setView([lat, lon]);
+  currentMarker = L.marker([lat, lon]).addTo(map);
 }
 
-
-let map = L.map("map",{
-    dragging:false,
+let map = L.map("map", {
+  dragging: false,
 }).setView([51.505, -0.09], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "© OpenStreetMap contributors",
+  attribution: "© OpenStreetMap contributors",
 }).addTo(map);
 let currentMarker = L.marker([51.505, -0.09]).addTo(map);
 L.Control.geocoder({
-    defaultMarkGeocode: true,
+  defaultMarkGeocode: true,
 }).addTo(map);
 
-
-
-
-
 function setDayAndTime(data) {
-    const dateTime = new Date();
-    let numOfDay = dateTime.getDay();
-    let dateNum = dateTime.getDate();
-    let hours = dateTime.getHours();
-    let minutes = dateTime.getMinutes();
-    let hrmn;
-    hrmn = `${hours}:${minutes <= 9 ? "0" + minutes: minutes}`;
-    days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-    let currentDay = days[numOfDay];
-    day.textContent = currentDay;
-    date.textContent = dateNum;
-    time.textContent = hrmn;
-    mainImg.src = data.condition.icon;
-    subImg.src = data.condition.icon;
-    tempImg.src = data.condition.icon;
-    condText.textContent = data.condition.text;
-    //set temperature 
-    console.log(data)
-    temp.textContent=`${data.temp_c}°C`
+  const dateTime = new Date();
+  let numOfDay = dateTime.getDay();
+  let dateNum = dateTime.getDate();
+  let hours = dateTime.getHours();
+  let minutes = dateTime.getMinutes();
+  let hrmn;
+  hrmn = `${hours}:${minutes <= 9 ? "0" + minutes : minutes}`;
+  days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let currentDay = days[numOfDay];
+  day.textContent = currentDay;
+  date.textContent = dateNum;
+  time.textContent = hrmn;
+  mainImg.src = data.condition.icon;
+  subImg.src = data.condition.icon;
+  tempImg.src = data.condition.icon;
+  condText.textContent = data.condition.text;
+  //set temperature
+  console.log(data);
+  temp.textContent = `${data.temp_c}°C`;
 }
-function populateAirCondition(data){
-    wind.textContent=data.windchill_c
-    pressure.textContent=data.pressure_in
-    humidity.textContent=data.humidity
-}
-function createHourlyDiv(data) {
-    while (hourlyForecast.firstChild){
-        hourlyForecast.removeChild(hourlyForecast.firstChild)
-    }
-    for (i=0;i < data[0].hour.length-1;i++) {
-        const hourlyDiv = document.createElement("div");
-        hourlyForecast.appendChild(hourlyDiv);
-    }
-    const childDivs = hourlyForecast.querySelectorAll(".next_23_hours >*");
-    childDivs.forEach((hour, index) => {
-        if (data[0].hour[index]) {
-           const temp = document.createElement("span");
-            const img = document.createElement("img");
-            const time = document.createElement("span");
-            temp.textContent = `${data[0].hour[index].temp_c}°c`;
-            img.src = data[0].hour[index].condition.icon;
-            index = index === 0 ? index + 1: index;
-            const timeString = data[0].hour[index].time;
-            console.log(timeString);
-            const splitTime = timeString.split(" ");
-            time.textContent = splitTime[1];
 
-            hour.appendChild(temp);
-            hour.appendChild(img);
-            hour.appendChild(time);
-        }
-    });
+function populateAirCondition(data) {
+  console.log(data);
+  console.log("i was called");
+  wind.textContent = data.windchill_c;
+  console.log(data.windchill_c);
+  pressure.textContent = data.pressure_in;
+  humidity.textContent = data.humidity;
+}
+
+function createHourlyDiv(data) {
+  while (hourlyForecast.firstChild) {
+    hourlyForecast.removeChild(hourlyForecast.firstChild);
+  }
+  for (i = 0; i < data[0].hour.length - 1; i++) {
+    const hourlyDiv = document.createElement("div");
+    hourlyForecast.appendChild(hourlyDiv);
+  }
+  const childDivs = hourlyForecast.querySelectorAll(".next_23_hours >*");
+  childDivs.forEach((hour, index) => {
+    if (data[0].hour[index]) {
+      const temp = document.createElement("span");
+      const img = document.createElement("img");
+      const time = document.createElement("span");
+      temp.textContent = `${data[0].hour[index].temp_c}°c`;
+      img.src = data[0].hour[index].condition.icon;
+      index = index === 0 ? index + 1 : index;
+      const timeString = data[0].hour[index].time;
+      const splitTime = timeString.split(" ");
+      time.textContent = splitTime[1];
+
+      hour.appendChild(temp);
+      hour.appendChild(img);
+      hour.appendChild(time);
+    }
+  });
 }
 
 // function populateDailyForecast(data) {
@@ -146,4 +144,3 @@ function createHourlyDiv(data) {
 //     dailyForecast.appendChild(divDay);
 //   }
 // }
-
